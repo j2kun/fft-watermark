8bc668c3a01e97623dc445a4b62077bac277080d
Nariman Saftarli <saftarli.n@gmail.com>
2021-03-30 00:40:55 -0400

    Make workable with Python 3 and imageio
    Also:
    * Linting
    * gitignores

--- fft.py
+++ fft.py
@@ -3,71 +3,73 @@ import numpy
 import matplotlib.pyplot as plt
 import random
 
+
 def memoize(f):
-   cache = {}
+    cache = {}
 
-   def memoizedFunction(*args):
-      if args not in cache:
-         cache[args] = f(*args)
-      return cache[args]
+    def memoizedFunction(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
 
-   memoizedFunction.cache = cache
-   return memoizedFunction
+    memoizedFunction.cache = cache
+    return memoizedFunction
 
 
 @memoize
 def omega(p, q):
-   return cmath.exp((2.0 * cmath.pi * 1j * q) / p)
+    return cmath.exp((2.0 * cmath.pi * 1j * q) / p)
 
 
 def pad(inputList):
-   k = 0
-   while 2**k < len(inputList):
-      k += 1
-   return numpy.concatenate((inputList, ([0] * (2**k - len(inputList)))))
+    k = 0
+    while 2**k < len(inputList):
+        k += 1
+    return numpy.concatenate((inputList, ([0] * (2**k - len(inputList)))))
 
 
 def fft(signal):
-   n = len(signal)
-   if n == 1:
-      return signal
-   else:
-      Feven = fft([signal[i] for i in xrange(0, n, 2)])
-      Fodd = fft([signal[i] for i in xrange(1, n, 2)])
+    n = len(signal)
+    if n == 1:
+        return signal
+    else:
+        Feven = fft([signal[i] for i in xrange(0, n, 2)])
+        Fodd = fft([signal[i] for i in xrange(1, n, 2)])
 
-      combined = [0] * n
-      for m in xrange(n/2):
-         combined[m] = Feven[m] + omega(n, -m) * Fodd[m]
-         combined[m + n/2] = Feven[m] - omega(n, -m) * Fodd[m]
+        combined = [0] * n
+        for m in xrange(n/2):
+            combined[m] = Feven[m] + omega(n, -m) * Fodd[m]
+            combined[m + n/2] = Feven[m] - omega(n, -m) * Fodd[m]
 
-      return combined
+        return combined
 
 
 def fft2d(matrix):
-   fftRows = numpy.array([fft(row) for row in matrix])
-   return numpy.array([fft(row) for row in fftRows.transpose()]).transpose()
+    fftRows = numpy.array([fft(row) for row in matrix])
+    return numpy.array([fft(row) for row in fftRows.transpose()]).transpose()
 
 
 def testCase():
-   A = numpy.array([[0,0,0,0], [0,1,0,0], [0,0,0,0], [0,0,0,0]])
-   for row in fft2d(A):
-      print(', '.join(['%.3f + %.3fi' % (x.real, x.imag) for x in row]))
+    A = numpy.array([[0, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 0, 0],
+                     [0, 0, 0, 0]])
+    for row in fft2d(A):
+        print(', '.join(['%.3f + %.3fi' % (x.real, x.imag) for x in row]))
 
 
-norm = lambda x: cmath.polar(x)[0]
+# norm = lambda x: cmath.polar(x)[0]
 
 # playing with images
 
 def frequencyFilter(signal):
-   for i in range(20000, len(signal)-20000):
-      signal[i] = 0
+    for i in range(20000, len(signal)-20000):
+        signal[i] = 0
 
 
 def processWithNumpy(imageData):
-   transformedImage = numpy.fft.fft2(imageData)
-   frequencyFilter(transformedImage)
+    transformedImage = numpy.fft.fft2(imageData)
+    frequencyFilter(transformedImage)
 
-   cleanedImage = numpy.fft.ifft2(transformedImage)
-   return numpy.array(cleanedImage, dtype=numpy.float64)
-
-
+    cleanedImage = numpy.fft.ifft2(transformedImage)
+    return numpy.array(cleanedImage, dtype=numpy.float64)
--- watermark.py
+++ watermark.py
@@ -4,56 +4,67 @@ from scipy import misc
 from scipy import ndimage
 import math
 import random
+import imageio
+
 
 def scaleSpectrum(A):
-   return numpy.real(numpy.log10(numpy.absolute(A) + numpy.ones(A.shape)))
+    return numpy.real(numpy.log10(numpy.absolute(A) + numpy.ones(A.shape)))
 
 
 def randomVector(seed, length):
-   random.seed(secretKey)
-   return [random.choice([0,1]) for _ in range(length)]
+    random.seed(secretKey)
+    return [random.choice([0, 1]) for _ in range(length)]
 
 
 def applyWatermark(imageMatrix, watermarkMatrix, alpha):
-   shiftedDFT = fftshift(fft2(imageMatrix))
-   watermarkedDFT = shiftedDFT + alpha * watermarkMatrix
+    shiftedDFT = fftshift(fft2(imageMatrix))
+    watermarkedDFT = shiftedDFT + alpha * watermarkMatrix
 
-   #misc.imsave("images/watermarked-spectrum.png", scaleSpectrum(watermarkedDFT))
-   watermarkedImage = ifft2(ifftshift(watermarkedDFT))
+    watermarkedImage = ifft2(ifftshift(watermarkedDFT))
 
-   return watermarkedImage
+    return watermarkedImage
 
 
 def makeWatermark(imageShape, radius, secretKey, vectorLength=50):
-   watermark = numpy.zeros(imageShape)
-   center = (int(imageShape[0] / 2) + 1, int(imageShape[1] / 2) + 1)
+    watermark = numpy.zeros(imageShape)
+    center = (int(imageShape[0] / 2) + 1, int(imageShape[1] / 2) + 1)
 
-   vector = randomVector(secretKey, vectorLength)
+    vector = randomVector(secretKey, vectorLength)
 
-   x = lambda t: center[0] + int(radius * math.cos(t * 2 * math.pi / vectorLength))
-   y = lambda t: center[1] + int(radius * math.sin(t * 2 * math.pi / vectorLength))
-   indices = [(x(t), y(t)) for t in range(vectorLength)]
+    indices = [watermarkValue(center,
+                              t,
+                              vectorLength,
+                              radius) for t in range(vectorLength)]
 
-   for i,location in enumerate(indices):
-      watermark[location] = vector[i]
+    for i, location in enumerate(indices):
+        watermark[location] = vector[i]
 
-   return watermark
+    return watermark
+
+
+def watermarkValue(center, value, vectorLength, radius):
+    return (center[0] + int(radius *
+                            math.cos(2 * value * math.pi / vectorLength)),
+            center[1] + int(radius *
+                            math.sin(2 * value * math.pi / vectorLength)))
 
 
 def decodeWatermark(image, secretKey):
-   pass
+    pass
 
 
 if __name__ == "__main__":
-   filename = "images/hance-up-sw.jpg"
-   secretKey = 57846874321257
-   alpha = 0
+    filename = "images/hance-up-sw.jpg"
+    secretKey = 57846874321257
+    alpha = 0
 
-   theImage = ndimage.imread(filename, flatten=True)
-   watermark = makeWatermark(theImage.shape, min(theImage.shape) / 4, secretKey=secretKey)
+    theImage = imageio.imread(filename, as_gray=True)
+    watermark = makeWatermark(theImage.shape,
+                              min(theImage.shape) / 4,
+                              secretKey=secretKey)
 
-   misc.imsave("images/watermark-spectrum.png", watermark)
+    imageio.imsave("images/watermark-spectrum.png", watermark)
 
-   watermarked = numpy.real(applyWatermark(theImage, watermark, alpha))
-   misc.imsave("%s-watermarked-%.3f.png" % (filename.split('.')[0], alpha), watermarked)
-
+    watermarked = numpy.real(applyWatermark(theImage, watermark, alpha))
+    imageio.imsave("%s-watermarked-%.3f.png" %
+                   (filename.split('.')[0], alpha), watermarked)
